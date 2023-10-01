d9f52de6ebd9fee090dfcd49a461249295ab9da2
GawronDev <kubusgawron@gmail.com>
2023-10-01 16:33:56 +0200

    Dropdown menu overlay added

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
+import "../css/menu.css";
 
 export default function Menu(props){
     useEffect(()=>{
+        if(props.status == "open") {
+            document.getElementById("menu").style.height = "100%";
+        } else if (props.status == "closed") {
+            document.getElementById("menu").style.height = "0%";
+        }
     },[props.status]);
 
     return (
-    <div className="menu">
+    <div className="menu" id="menu">
 
     </div>
     );
